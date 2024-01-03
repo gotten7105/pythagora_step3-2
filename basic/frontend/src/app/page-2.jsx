ec34d385c4e635a4ec0dada27215753c1d42dffa
tear0mark2@icloud.com <tear0mark2@icloud.com>
2024-01-03 23:14:18 +0900

    ページ遷移機能追加　usestateからuseFormに変更

--- basic/frontend/src/app/page-2.jsx
+++ basic/frontend/src/app/page-2.jsx
@@ -1,22 +1,16 @@
 "use client";
 
-import { useRef } from 'react';
-import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 function App() {
-  const formRef = useRef();
-  const router = useRouter();
-  const handleSend = async (event) => {
-    event.preventDefault();
-    const formData = new FormData(formRef.current);
-    const body_msg = JSON.stringify({
-      mail_address: formData.get("mail_address"),
-      password: formData.get("password"),
-  });
-    console.log(body_msg);
+  const handleSend = async (e) => {
+    e.preventDefault();
+
+    const formData = JSON.stringify(form);
+    console.log(form);
     const response = await fetch('http://127.0.0.1:5000/login', {
       method: 'POST',
-      body: body_msg,
+      body: formData,
       headers: {
         'Content-Type': 'application/json',
       },
@@ -25,23 +19,34 @@ function App() {
     if (response.ok) {
       console.log('送信成功');
       const jsonData = await response.json();
-      console.log(jsonData);
-      router.push("http://localhost:3000/main");
+      console.log(jsonData); // ← 追加
+      window.location.href = "http://localhost:3000/main";
     } else {
       console.log('送信失敗:', response.statusText);
     }
   };
 
+  const [form, setForm] = useState({ mail_address: '', password: '' });
+  const handleChange = (e) => {
+    setForm((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
   return (
     <div style={{ textAlign: 'center', marginTop: '2em' }}>
-      <h1>Welcome back to Cheers!</h1>
-      <form ref={formRef} onSubmit={handleSend}>
+      <h1>useStateでフォーム</h1>
+      <form onSubmit={handleSend}>
         <div>
           <label>
             メールアドレス:
             <input
               name="mail_address"
               type="email"
+              onChange={handleChange}
             />
           </label>
         </div>
@@ -51,10 +56,11 @@ function App() {
             <input
               name="password"
               type="password"
+              onChange={handleChange}
             />
           </label>
         </div>
-        <button type="submit">SIGN IN</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
